57bc40ccb3c8e74a37f7334f1703e454d98c2f1f
Hyroza <hyroza@gmail.com>
2021-04-15 11:42:21 +0200

    File

---
+++ HelloThere.java
@@ -0,0 +1,26 @@
+public class HelloThere {
+    	public static void main(String[] args) {
+
+            // Leistungsnachweis für Aufgabe 1:
+            int a = 0;
+            int b = 69;
+            int c = 420;
+            int Testnummer = 0;
+            
+            Testnummer = b + c;
+
+            System.out.println(Testnummer);
+
+            
+            // Leistungsnachweis für Aufgabe 4:
+            int d = 10;
+            int e = 15;
+            int f = 20;
+            int summe = 0;
+            
+            summe = (int) ((Math.random() * (1000 - 1)) + 1 + 1);
+
+            System.out.println(summe);
+        }
+
+}
--- retardgrad.py
+++ retardgrad.py
@@ -9,50 +9,52 @@
 
 # # age = string
 
-def isnan(input):
-    try:
-        input = int(input)
-        return True
 
-    except expression as identifier:
-        return False
+# def isnan(input):
+#     try:
+#         input = int(input)
+#         return True
 
-
-
-
-age = input("Gebe dein Alter ein: ")
-
-if isnan(age) != True and isnan(retard_grad) != True and isnan(verschwendete_zeit) != True:
-    print("Gebe Zahlen ein du Mongo")
-
-age = int(age)
+#     except:
+#         return False
 
 
-retard_grad = input("Geben sie den Grad ihrer Behinderung ein: ")
-
-retard_grad = int(retard_grad)
 
 
-verschwendete_zeit = input("Wie lange sind Sie schon im Unternehmen tätig?: ")
-
-verschwendete_zeit = int(verschwendete_zeit)
 
 
-urlaubstage = 26
+ age = input("Gebe dein Alter ein: ")
 
-if age < 18:
-    urlaubstage = 30
+ retard_grad = input("Geben sie den Grad ihrer Behinderung ein: ")
 
-if age > 55:
-    urlaubstage = urlaubstage +2
+ verschwendete_zeit = input("Wie lange sind Sie schon im Unternehmen tätig?: ")
 
-if retard_grad > 50:
-    urlaubstage = urlaubstage +5
 
-if verschwendete_zeit > 10:
-    urlaubstage = urlaubstage + 2
+ if isnan(age) == False or isnan(retard_grad) == False or isnan(verschwendete_zeit) == False:
+     print("Gebe Zahlen ein du Mongo")
+ else: 
+     age = int(age)
 
-print(urlaubstage)
+     retard_grad = int(retard_grad)
+
+     verschwendete_zeit = int(verschwendete_zeit)
+
+
+     urlaubstage = 26
+
+     if age < 18:
+         urlaubstage = 30
+
+     if age > 55:
+         urlaubstage = urlaubstage + 2
+
+     if retard_grad > 50:
+         urlaubstage = urlaubstage + 5
+
+     if verschwendete_zeit > 10:
+         urlaubstage = urlaubstage + 2
+
+     print(urlaubstage)
 
 
 
